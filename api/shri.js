0ea7511487703632b9c83819f2bcc37f929ed5aa
Dimitriy Leonov <dudv@ya.ru>
2015-08-13 17:40:41 +0300

    Initial commit, all the tasks are done.

---
+++ api/shri.js
@@ -0,0 +1,138 @@
+/**
+ * Реализация API, не изменяйте ее
+ * @param {string} url
+ * @param {function} callback
+ */
+function getData(url, callback) {
+    var RESPONSES = {
+        '/countries': [
+            {name: 'Cameroon', continent: 'Africa'},
+            {name :'Fiji Islands', continent: 'Oceania'},
+            {name: 'Guatemala', continent: 'North America'},
+            {name: 'Japan', continent: 'Asia'},
+            {name: 'Yugoslavia', continent: 'Europe'},
+            {name: 'Tanzania', continent: 'Africa'}
+        ],
+        '/cities': [
+            {name: 'Bamenda', country: 'Cameroon'},
+            {name: 'Suva', country: 'Fiji Islands'},
+            {name: 'Quetzaltenango', country: 'Guatemala'},
+            {name: 'Osaka', country: 'Japan'},
+            {name: 'Subotica', country: 'Yugoslavia'},
+            {name: 'Zanzibar', country: 'Tanzania'},
+        ],
+        '/populations': [
+            {count: 138000, name: 'Bamenda'},
+            {count: 77366, name: 'Suva'},
+            {count: 90801, name: 'Quetzaltenango'},
+            {count: 2595674, name: 'Osaka'},
+            {count: 100386, name: 'Subotica'},
+            {count: 157634, name: 'Zanzibar'}
+        ]
+    };
+
+    setTimeout(function () {
+        var result = RESPONSES[url];
+        if (!result) {
+            return callback('Unknown url');
+        }
+
+        callback(null, result);
+    }, Math.round(Math.random * 1000));
+}
+
+/**
+ * Ваши изменения ниже
+ */
+var requests = ['/countries', '/cities', '/populations'];
+var responses = {};
+
+for (i = 0; i < 3; i++) {    
+    /* 
+        Ошибка состоит в неправильном использовании замыканий.
+        Функция callback запускается не сразу, а спустя некоторый промежуток времени.
+        При этом значение переменной request она берёт из внешнего scope.
+        В момент выполнения функции callback цикл уже заканчивается, и во внешнем scope
+        переменная request имеет значение '/populations' - как последнего из 3 запросов.
+        Поэтому объект responses содержит всегда одно поле, а значит, длина массива l никогда не
+        достигает 3.
+        Для исправления ошибки я создал дополнительный scope внутри функции-обёртки,
+        который сохраняет правильное значение переменной request.
+    */    
+    var request = requests[i];
+    var callback = function(request) {
+        return function (error, result) {
+            responses[request] = result;
+            var l = [];
+            for (K in responses)
+                l.push(K);
+
+            if (l.length == 3) {
+                var c = [], cc = [], p = 0;
+                for (i = 0; i < responses['/countries'].length; i++) {
+                    if (responses['/countries'][i].continent === 'Africa') {
+                        c.push(responses['/countries'][i].name);
+                    }
+                }
+
+                for (i = 0; i < responses['/cities'].length; i++) {
+                    for (j = 0; j < c.length; j++) {
+                        if (responses['/cities'][i].country === c[j]) {
+                            cc.push(responses['/cities'][i].name);
+                        }
+                    }
+                }
+
+                for (i = 0; i < responses['/populations'].length; i++) {
+                    for (j = 0; j < cc.length; j++) {
+                        if (responses['/populations'][i].name === cc[j]) {
+                            p += responses['/populations'][i].count;
+                        }
+                    }
+                }
+
+                console.log('Total population in African cities: ' + p);
+            }
+        };
+    } (request);
+
+    getData(request, callback);
+}
+
+function getCityPopulation(cityName) {
+    var cityPopulations = responses['/populations'];
+    for (var i = 0; i < cityPopulations.length; ++i) {
+        if (cityPopulations[i].name == cityName) {
+            return cityPopulations[i].count;
+        }
+    }
+    return null;
+}
+
+function makeCityPopulationQuery() {
+    var cityName = window.prompt('Enter city name', 'Osaka');
+    var population = getCityPopulation(cityName);
+    if (population)
+        alert(cityName + ' population is ' + population);
+    else
+        alert('City not found');
+}
+
+function makeCountryPopulationQuery() {
+    var countryName = window.prompt('Enter country name', 'Japan');
+    var cities = responses['/cities'], countryCities = [], i;
+    for (i = 0; i < cities.length; ++i) {
+        if (cities[i].country == countryName) {
+           countryCities.push(cities[i].name);
+        }
+    }
+    if (countryCities.length == 0) {
+        alert('Country not found');
+        return;
+    }
+    var totalPopulation = 0;
+    for (i = 0; i < countryCities.length; ++i) {
+        totalPopulation += getCityPopulation(countryCities[i]);
+    }
+    alert(countryName + ' population is ' + totalPopulation);
+}
---
+++ player/player.js
@@ -0,0 +1,183 @@
+(function () {	
+
+var context, source, soundDataBuffer, analyser, equalizerFilters, spectrumNode;
+window.addEventListener('load', function() {
+	try {
+		context = new AudioContext();
+		setupSpectrum();
+		createEqualizerFilters();		
+	}
+	catch (e) {
+		alert('Your browser does not support Web Audio API');
+	}
+});
+
+var playButton = document.getElementById('play-button'),
+	trackTitleDiv = document.getElementById('track-title'),
+	filenameDiv = document.getElementById('filename'),
+	soundInput = document.getElementById('sound-input'),
+	dropRegion = document.getElementById('drop-region'),
+	equalizer = document.getElementById('equalizer'),
+	spectrumCanvas = document.getElementById('spectrum-canvas'),
+	player = document.getElementById('player');
+
+function loadSoundFile(file) {
+	var fileReader = new FileReader();	
+	fileReader.onload = function(e) {		
+		context.decodeAudioData(this.result, function(buffer) {
+			soundDataBuffer	= buffer;
+			extractMetadata(file);
+			playSound();			
+		}, function(e) {
+			trackTitleDiv.innerHTML = 'Error decoding file';
+			stopSound();
+			player.setAttribute('data-state', 'no-track');
+		});
+	};	
+	fileReader.readAsArrayBuffer(file);
+}
+
+function playSound() {
+	if (player.getAttribute('data-state') == 'playing')
+		stopSound();
+
+	source = context.createBufferSource(); 
+	source.buffer = soundDataBuffer;	                 
+	connectEqualizerFilters();
+	connectSpectrumNode();
+	source.start(0);
+
+	player.setAttribute('data-state', 'playing');
+}
+
+function stopSound() {
+	source.stop();
+
+	player.setAttribute('data-state', 'stopped');
+}
+
+
+soundInput.addEventListener('change', function (e) {
+	if (e.target.files.length)
+		loadSoundFile(e.target.files[0]);
+});
+
+playButton.addEventListener("click", function() {
+	var playerState = player.getAttribute('data-state');
+	if (playerState == 'no-track') {
+		var mouseEvent = document.createEvent("MouseEvents");
+	    mouseEvent.initMouseEvent("click", true, true, window, 1, 0, 0, 0, 0,
+	        false, false, false, false, 0, null);
+	    soundInput.dispatchEvent(mouseEvent);
+	} else if (playerState == 'playing') {
+		stopSound();
+	} else if (playerState == 'stopped') {
+		playSound();
+	}
+});
+
+function extractMetadata(file) {
+	var success_callback = function (metadata) {
+		trackTitleDiv.innerHTML = metadata.artist + ' - ' + metadata.title;
+		filenameDiv.innerHTML = file.name;
+	};
+	var error_callback = function (e) {
+	    console.log(e);
+	};
+	parse_audio_metadata(file, success_callback, error_callback)
+} 
+
+
+function onDragover(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	e.dataTransfer.dropEffect = 'copy';
+}
+
+function onDrop(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	if (e.dataTransfer.files.length)
+		loadSoundFile(e.dataTransfer.files[0]);
+}
+
+dropRegion.addEventListener('dragover', onDragover);
+player.addEventListener('dragover', onDragover);
+dropRegion.addEventListener('drop', onDrop);
+player.addEventListener('drop', onDrop);
+
+function createEqualizerFilters () {
+	var frequencies = [60, 170, 310, 600, 1000, 3000, 6000, 12000, 14000, 16000];
+	equalizerFilters = frequencies.map(function (frequency) {
+	  var filter = context.createBiquadFilter();	     
+	  filter.type = 'peaking';
+	  filter.frequency.value = frequency;
+	  filter.Q.value = 1;
+	  filter.gain.value = 0;
+	  return filter;
+	});
+	equalizerFilters.reduce(function (previous, current) {
+		previous.connect(current);
+		return current;
+	});
+}
+
+function connectEqualizerFilters () {
+	source.connect(equalizerFilters[0]);
+	equalizerFilters[equalizerFilters.length - 1].disconnect();
+	equalizerFilters[equalizerFilters.length - 1].connect(context.destination);
+}
+
+equalizer.addEventListener('change', function (e) {
+	var equalizerPresets = {
+		'normal': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		'pop': [-1.125, 3, 4.5, 4.875, 3.375, -0.75, -1.5, -1.5, -1.125, -1.125],
+		'rock': [4.875, 3, -3.375, -4.875, -2.25, 2.625, 5.625, 6.75, 6.75, 6.75],
+		'jazz': [-1.5, -3, -2.625, -0.375, 2.625, 3.75, 5.625, 6, 6.75, 6],
+		'classic': [0.375, 0.375, 0.375, 0.375, 0.375, 0.375, -4.5, -4.5, -4.5, -6]
+	}
+	var preset = equalizerPresets[e.target.value];
+	for (var i = 0; i < equalizerFilters.length; ++i) {
+		equalizerFilters[i].gain.value = preset[i];
+	}
+});
+
+function setupSpectrum () {
+	analyser = context.createAnalyser();
+	analyser.smoothingTimeConstant = 0.3;
+    analyser.fftSize = 1024;
+	spectrumNode = context.createScriptProcessor(0, 1, 1);
+
+	var canvasContext = spectrumCanvas.getContext('2d');
+	spectrumNode.onaudioprocess = function() {
+		var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+ 		
+ 		var w = window.innerWidth, h = Math.max(window.innerHeight, 256);
+ 		spectrumCanvas.width = w;
+ 		spectrumCanvas.height = h;
+ 		canvasContext.clearRect(0, 0, w, h);
+
+		var gradient = canvasContext.createLinearGradient(0, 0, 0, h);
+		gradient.addColorStop(1, '#000000');
+		gradient.addColorStop(1 - 128.0 / h, '#e61400');
+		gradient.addColorStop(1 - 256.0 / h, '#ffffff');
+		gradient.addColorStop(0, '#ffffff');
+ 		canvasContext.fillStyle = gradient;
+ 		var barWidth = Math.max(Math.floor(w / array.length), 2), barWidthSpaced = Math.max(barWidth - 1, 1);
+ 		for (var i = 0; i < array.length; ++i) {
+			canvasContext.fillRect(barWidth * i, h - array[i], barWidthSpaced, h);			
+ 		}
+	};
+}
+
+function connectSpectrumNode() {
+	spectrumNode.disconnect();
+	analyser.disconnect();
+	spectrumNode.connect(context.destination);	
+	equalizerFilters[equalizerFilters.length - 1].connect(analyser);
+	analyser.connect(spectrumNode);
+}
+
+
+})();
